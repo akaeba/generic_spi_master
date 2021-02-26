14a35019713d0005b6ba23cff1221d3c41e2294c
Andreas Kaeberlein <andreas.kaeberlein@web.de>
2021-02-26 17:53:46 +0100

    sim scripts

---
+++ tcl/sim/generic_spi_master_inp_filter/generic_spi_master_inp_filter_runsim.tcl
@@ -0,0 +1,27 @@
+##************************************************************************
+## @author:         Andreas Kaeberlein
+## @copyright:      Copyright 2021
+## @credits:        AKAE
+##
+## @license:        BSDv3
+## @maintainer:     Andreas Kaeberlein
+## @email:          andreas.kaeberlein@web.de
+##
+## @file:           generic_spi_master_inp_filter_runsim.tcl
+## @date:           2021-02-26
+##
+## @brief:          starts simulation
+##
+##					Modelsim: Tools -> TCL -> Execute Macro
+##************************************************************************
+
+
+
+# start simulation, disable optimization
+vsim -novopt -gDO_ALL_TEST=true -t 1ps work.generic_spi_master_inp_filter_tb
+
+# load Waveform
+do "../tcl/sim/generic_spi_master_inp_filter/generic_spi_master_inp_filter_waveform.do"
+
+# sim until finish
+#run 3.5 ms
