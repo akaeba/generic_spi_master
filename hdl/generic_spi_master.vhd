b76ec24169138de5186ac94afb8efbd7e0b22ea0
Andreas Kaeberlein <andreas.kaeberlein@web.de>
2021-02-28 19:42:08 +0100

    add some sim/syn messages

--- hdl/generic_spi_master.vhd
+++ hdl/generic_spi_master.vhd
@@ -185,9 +185,25 @@ begin
 
     ----------------------------------------------
     -- Synthesis/Simulator Messages
-
-
-
+    -- general
+    assert not true
+    report                                                                        character(LF) &
+            "generic_spi_master " & generic_spi_master'path_name                & character(LF) &
+            "  SPI Mode    : " & integer'image(SPI_MODE)                        & character(LF) &
+            "  Chip select : " & integer'image(NUM_CS)                          & character(LF) &
+            "  SFR width   : " & integer'image(DW_SFR)                          & character(LF) &
+            "  FCLK        : " & integer'image(CLK_HZ) & "Hz"                   & character(LF) &
+            "  FSCK        : " & integer'image(CLK_HZ/(2*c_sck_div_2)) & "Hz"
+    severity note;
+    -- filter not implemented, but requested
+    assert not ( (c_sck_div_2 <= (MISO_SYNC_STG + MISO_FILT_STG)) and ((0 /= MISO_SYNC_STG) or (0 /= MISO_FILT_STG)) )
+    report                                                            character(LF) &
+            "MISO Input Filter " & generic_spi_master'path_name     & character(LF) &
+            "  SYNC        : " & integer'image(MISO_SYNC_STG)       & character(LF) &
+            "  FILTER      : " & integer'image(MISO_FILT_STG)       & character(LF) &
+            "  CLKDIV2     : " & integer'image(c_sck_div_2)         & character(LF) &
+            "NOT IMPLEMENTED, OVERSAMPLING FACTOR OF SCK TWO LOW; SYNC + FILTER < CLKDIV2"
+    severity warning;
     ----------------------------------------------
 
 
@@ -351,7 +367,7 @@ begin
 
         --***************************
         -- No input filtering
-        --
+        --   filter delay is longer then capturing state of FSM, avoids complex FSM
         g_skip_filter : if c_sck_div_2 <= (MISO_SYNC_STG + MISO_FILT_STG) generate
             miso_filt   <= MISO;
             miso_shift  <= miso_shift_in;
